670d85e228db159d1ab3fcdf62699ebb95a8f082
pradeepkumarpawar <pradeepkumar.pawar87@gmail.com>
2021-09-22 13:46:31 +0530

    Commit in feature-A

---
+++ src/com/fsg/Test2.java
@@ -0,0 +1,10 @@
+package com.fsg;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this test 2 added in new branch ...");
+	}
+
+}
